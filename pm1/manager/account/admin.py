f33915f4b6cde0cbc735c2afae8860b5a32ec320
cap7nsaurabh <saurabh.singh3@globallogic.com>
2022-03-07 11:32:22 +0530

    pushing final proect

--- pm1/manager/account/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from account.models import cards,Entry,Meetings
-# Register your models here.
-admin.site.register(cards)
-admin.site.register(Entry)
-admin.site.register(Meetings)
